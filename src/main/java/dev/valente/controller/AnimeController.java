8ef9410f6a2cf167282f73064052c2632d1f3ce5
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2024-12-15 17:38:53 -0300

    Aula 10: threads virtuais e teste de performance

--- src/main/java/dev/valente/controller/AnimeController.java
+++ src/main/java/dev/valente/controller/AnimeController.java
@@ -2,6 +2,8 @@ package dev.valente.controller;
 
 import dev.valente.dto.AnimeDTO;
 import jakarta.servlet.http.HttpServletRequest;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,16 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/v1/animes")
-public class HelloController {
+public class AnimeController {
 
     @GetMapping
     public ResponseEntity<AnimeDTO> listAll(HttpServletRequest request) {
         List<String> list = List.of("Naruto", "DBZ", "Inuyasha", "Digimon");
         var uri = request.getRequestURI();
         var dto = new AnimeDTO(list, uri);
-
+        log.info(Thread.currentThread().getName());
         return ResponseEntity.ok().body(dto);
     }
 
