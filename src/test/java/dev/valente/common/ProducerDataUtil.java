63be7f8cf7c0346d51b4cc3cc7eb3f2d72308031
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2024-12-29 20:42:15 -0300

    29/12

--- src/test/java/dev/valente/common/ProducerDataUtil.java
+++ src/test/java/dev/valente/common/ProducerDataUtil.java
@@ -1,21 +1,28 @@
 package dev.valente.common;
 
 import dev.valente.producer.domain.Producer;
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Getter
 public class ProducerDataUtil implements DataUtil<Producer> {
 
     private final List<Producer> PRODUCER_LIST = new ArrayList<>();
 
+    private final DateTimeFormatter DATA_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSS");
+
+    private final LocalDateTime DATA = LocalDateTime.parse("2024-12-29T20:00:54.0151691", DATA_FORMATTER);
+
     {
-        PRODUCER_LIST.add(Producer.builder().id(1L).name("Mappa").createdAt(LocalDateTime.now()).build());
-        PRODUCER_LIST.add(Producer.builder().id(2L).name("Kyoto Animation").createdAt(LocalDateTime.now()).build());
-        PRODUCER_LIST.add(Producer.builder().id(3L).name("Mad House").createdAt(LocalDateTime.now()).build());
+        PRODUCER_LIST.add(Producer.builder().id(1L).name("Animation").createdAt(DATA).build());
+        PRODUCER_LIST.add(Producer.builder().id(2L).name("Kyoto").createdAt(DATA).build());
+        PRODUCER_LIST.add(Producer.builder().id(3L).name("House").createdAt(DATA).build());
     }
 
     @Override
--- src/test/java/dev/valente/producer/controller/ProducerControllerTest.java
+++ src/test/java/dev/valente/producer/controller/ProducerControllerTest.java
@@ -1,7 +1,65 @@
 package dev.valente.producer.controller;
 
+import dev.valente.common.DataUtil;
+import dev.valente.producer.domain.Producer;
+import dev.valente.producer.repository.ProducerData;
+import dev.valente.producer.repository.ProducerHardCodedRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest(controllers = ProducerController.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ComponentScan("dev.valente")
+//@Import({...})
 class ProducerControllerTest {
 
+    @InjectMocks
+    private ProducerController producerController;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProducerData producerData;
+
+    @Autowired
+    private DataUtil<Producer> dataUtil;
+
+    @Test
+    @DisplayName("GET v1/producers should return list of all producers")
+    void findAll_shouldReturnListOfproducers() throws Exception {
+
+        BDDMockito.when(producerData.getProducers()).thenReturn(dataUtil.getList());
+
+        var texto = Files.readString(Paths.get("src/test/resources/get_allproducers_200.json"));
+
+
+        var result = mockMvc.perform(MockMvcRequestBuilders.get("/v1/producers")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(texto))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(dataUtil.getList().size()));
+
+    }
 }
